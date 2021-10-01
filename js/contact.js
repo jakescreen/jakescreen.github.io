34a1e407627fdaa8c7d2f439cd7c935fa838f4f8
jakescreen <jscreen@udel.edu>
2021-09-30 23:21:23 -0400

    hw2 complete

--- js/contact.js
+++ js/contact.js
@@ -19,3 +19,26 @@
       })
   })()
 
+
+window.onload = function () {
+    document.getElementById('nameform').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.send('service_u4xvp73', 'template1234', {
+            user_fname: document.getElementById('user_fname').value,
+            user_lname: document.getElementById('user_lname').value,
+            user_email: document.getElementById('user_email').value,
+            user_phone: document.getElementById('user_phone').value,
+            user_message: document.getElementById('user_message').value,
+        }).then(function(response){
+            console.log('SUCCESS!', response.status, response.text);
+            document.getElementById("jumbotron").innerHTML = "SUCCESS!";
+        }, function(error){
+            console.log('FAILED...', error);
+            document.getElementById("jumbotron").innerHTML = "FAILED!" + error;
+        });
+            
+    });
+}
